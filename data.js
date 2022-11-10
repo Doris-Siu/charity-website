48abf3921a62a05b965e95f5903dd2b14a773f08
Doris-Siu <doris.siu.my@gmail.com>
2022-11-10 14:10:01 +0000

    Create new repo for buddy project

---
+++ data.js
@@ -0,0 +1,29 @@
+function getAllAnimals() {
+  return [
+    {
+      id: 1,
+      name: "",
+      url:"",
+      service: "",
+      image: "",
+      summary:"",
+      
+    },
+    {
+      id: 2,
+      name: "",
+      url:"",
+      service: "",
+      image: "",
+      summary:"",
+    },
+    {
+      id: 3,
+      name: "",
+      url:"",
+      service: "",
+      image: "",
+      summary:"",
+    }
+  ];
+}
